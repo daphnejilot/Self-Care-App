c88ec72699cecadac56d41f36d2861fa1a5b5795
daphnejilot <daphnejilot@gmail.com>
2025-04-28 12:57:51 -0700

    Initial Commit

---
+++ Self Care App/Self_Care_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Self_Care_AppApp.swift
+//  Self Care App
+//
+//  Created by Daphne Jilot on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Self_Care_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Self Care AppTests/Self_Care_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Self_Care_AppTests.swift
+//  Self Care AppTests
+//
+//  Created by Daphne Jilot on 4/28/25.
+//
+
+import Testing
+@testable import Self_Care_App
+
+struct Self_Care_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
