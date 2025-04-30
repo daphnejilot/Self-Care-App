4230916ab09519355bb6958c2e33d746813ababb
daphnejilot <daphnejilot@gmail.com>
2025-04-30 13:45:04 -0700

    Added text

--- Self Care App/ContentView.swift
+++ Self Care App/ContentView.swift
@@ -1,18 +1,42 @@
 //
 //  ContentView.swift
 //  Self Care App
-//
-//  Created by Daphne Jilot on 4/28/25.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("garden")
-            .resizable()
-            .scaledToFill()
-            .ignoresSafeArea()
+        ZStack {
+            Image("garden")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            Text("My Garden")
+                .font(.custom("Pixelated Elegance", size: 42))
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .shadow(color: .black.opacity(1), radius: 0.5, x: 1, y: 1)
+                .offset(x: 3, y: -280)
+            Text("My Garden")
+                .font(.custom("Pixelated Elegance", size: 42))
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .shadow(color: .black.opacity(1), radius: 0.5, x: 1, y: 1)
+                .offset(x: -3, y: -280)
+            Text("My Garden")
+                .font(.custom("Pixelated Elegance", size: 42))
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .shadow(color: .black.opacity(1), radius: 0.5, x: 1, y: 1)
+                .offset(x: 1, y: -283)
+            Text("My Garden")
+                .font(.custom("Pixelated Elegance", size: 42))
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .shadow(color: .black.opacity(1), radius: 0.5, x: 1, y: 1)
+                .offset(x: 1, y: -277)
+            Text("My Garden")
+                .font(.custom("Pixelated Elegance", size: 42))
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.278, saturation: 0.709, brightness: 0.718)/*@END_MENU_TOKEN@*/)
+                .shadow(color: .black.opacity(1), radius: 0.5, x: 1, y: 1)
+                .offset(y: -280)
+        }
     }
 }
 
